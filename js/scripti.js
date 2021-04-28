421b2eed1321e2cebe5758860de3cea852b58c1f
Pietari Tanner <cotapi01@students.oamk.fi>
2021-04-28 19:42:26 +0300

    index kuntoon, tietoja sivun navi, + muuta

--- js/scripti.js
+++ js/scripti.js
@@ -1,1 +1,28 @@
 /// <reference path="jquery-3.6.0.min.js" /> 
+
+let sivut = [
+    'THvisa.html',
+    'PTvisa.html',
+    'muistipeli.html'];
+
+let sivut2 = [
+    'KJvisa.html',
+    'PTvisa.html',
+    'PTristikko.html',
+    'muistipeli.html'
+];
+
+$(function () {
+    $("#arvo1").on("click", function(){
+        let sivu = Math.floor(Math.random(sivut) * 3);
+        window.open(sivut[sivu]);
+    });
+    $(function () {
+        $("#arvo2").on("click", function(){
+            let sivu = Math.floor(Math.random(sivut2) * 4);
+            window.open(sivut2[sivu]);
+        });
+    
+    }); 
+    
+}); 
