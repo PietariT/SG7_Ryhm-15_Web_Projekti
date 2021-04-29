c90324cb9f93c05fcd2573e308067767b0e2a7f8
Tero Hietala <n0hite00@students.oamk.fi>
2021-04-29 18:56:41 +0300

    fontteja, omia päivityksiä

--- js/TeronVisa.js
+++ js/TeronVisa.js
@@ -3,3 +3,48 @@
 
 
 
+// document ready function
+$(function () {
+
+    
+
+    
+    $("[name=kysymys1]").hide();
+    $("[name=kysymys2]").hide();
+    $("[name=kysymys3]").hide();
+    $("[name=kysymys4]").hide();
+    $("[name=kysymys5]").hide();
+
+
+    $("#aloitus").on("click", function () {
+        $("[name=kysymys1]").show(500);       
+            
+    });
+    
+    $("#seu1").on("click", function () {
+        $("[name=kysymys2]").show(500);       
+            
+    });
+
+    $("#seu2").on("click", function () {
+        $("[name=kysymys3]").show(500);       
+            
+    });
+
+    $("#seu3").on("click", function () {
+        $("[name=kysymys4]").show(500);       
+            
+    });
+
+    $("#seu4").on("click", function () {
+        $("[name=kysymys5]").show(500);       
+            
+    });
+
+
+
+
+});
+    
+    
+
